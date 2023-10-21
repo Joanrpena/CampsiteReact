5e3ecf4f96801b7b62caaf5c6db73c1ca7b6340e
Joan Pena <joan.r.pena1@gmail.com>
2023-10-21 00:59:38 -0400

    About us

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import { Container } from 'reactstrap';
+import DisplayList from '../features/display/DisplayList';
+
+
+const HomePage = () => {
+    return <div>
+        <Container>
+            <DisplayList />
+        </Container>
+    </div>;
+};
+
+export default HomePage;
